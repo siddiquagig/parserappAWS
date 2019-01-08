3e5f632b9f545037d178f199dd9c8474a55ba02e
ahmed531 <ahmedbeliever@gmail.com>
2019-01-08 09:38:37 +0400

    main parser application

---
+++ mysql.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "mypass";
+$file = fopen("./soh.csv","r");
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected successfully";
+mysqli_select_db($conn,"data");
+while(!feof($file))
+  {
+    $code = fgetcsv($file)[0];
+    $item = fgetcsv($file)[1];
+    $price = fgetcsv($file)[2];
+
+
+    if(trim($code) != "" && trim($item) != "" && trim($price) != ""){
+      $sql = "INSERT INTO appdata SET code='$code',item='$item',price='$price';";
+      //echo $sql;
+      if($conn->query($sql)){
+        echo "successfully";
+      }else{
+        echo("Error description: " . mysqli_error($conn));
+      }
+    }
+
+  }
+
+fclose($file);
+?>
